cf96333615d44ab6330f92c3c939a7b2c66c87e8
komoritakeshi <komoritakeshi@team-lab.com>
2022-03-21 15:57:16 +0900

    update

--- HogeLabelSampleProject/HogeLabelSampleProject/ViewController.swift
+++ HogeLabelSampleProject/HogeLabelSampleProject/ViewController.swift
@@ -6,17 +6,12 @@
 //
 
 import UIKit
-import HogeLabel
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        let label = HogeLabel(frame: CGRect(x: 100, y: 100, width: 100, height: 20))
-        label.hogeText = "test"
-        view.addSubview(label)
     }
 
 
